fdd06752777e130b5580261479833b1be7074f27
Mwangi Stanley <mwangistanley746.com>
2025-05-05 07:06:06 +0300

    Update firebase config and gitignore

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCc5ioxeD4aoSuivyiDTbtg6s5zCsBxaik",
-  authDomain: "vals-b6045.firebaseapp.com",
-  projectId: "vals-b6045",
-  storageBucket: "vals-b6045.firebasestorage.app",
-  messagingSenderId: "741266726490",
-  appId: "1:741266726490:web:a757ba476421ff8828c263",
-  measurementId: "G-BW0ZKYEG63"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
